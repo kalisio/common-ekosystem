c7a70599ab3eaf34ce5f401f3536da46508e39d3
cnouguier <ch.nouguier@gmail.com>
2026-01-31 00:12:33 +0100

    chore: updated build script

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 export const defaultConfig = {
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/index.js'),
       formats: ['es', 'cjs'],
       fileName: (format) => format === 'es' ? 'index.mjs' : 'index.cjs'
     },
