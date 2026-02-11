a46ff87151e4b165d67e3af90583ee9a36d195ba
cnouguier <ch.nouguier@gmail.com>
2026-02-11 09:47:13 +0100

    tests: fixed environement for graphiks

--- vitest.config.js
+++ vitest.config.js
@@ -40,6 +40,7 @@ export default defineConfig({
         test: {
           name: 'graphiks',
           root: 'packages/graphiks',
+          environment: 'happy-dom',
           include: ['test/**/*.js']
         }
       }
